d2ceeb4953333c570712b98136681c8d6d45bd84
Daniel Gealow <dgealow2@illinois.edu>
2019-06-12 22:57:52 -0500

    Added a react app folder

--- doggo/src/App.js
+++ doggo/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './StainedGlass.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Dan Gealow's Webpage</h1>
+      <h3>(A work in progress)</h3>
+      <h2>Praise the Sun!</h2>
+      <img src={logo} className="App-logo" alt="logo" />
+      { /*
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Praise the Sun!
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,6 +25,7 @@ function App() {
           Learn React
         </a>
       </header>
+        */ }
     </div>
   );
 }
